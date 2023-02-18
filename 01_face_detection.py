e57a67f5b12a095ebfa44cf78271ebd8f54a5488
alexmacharia <alex.machariam@gmail.com>
2023-02-18 06:21:35 +0300

    draw box around faces

--- 01_face_detection.py
+++ 01_face_detection.py
@@ -10,3 +10,18 @@ face_locations = face_recognition.face_locations(image)
 
 num_faces = len(face_locations)
 print("Found {} faces in this photo".format(num_faces))
+
+# Load image into PIL for drawing and displaying it
+pil_image = PIL.Image.fromarray(image)
+
+for location in face_locations:
+    # Print co-ordinates of each image
+    top, right, bottom, left = location
+    print("Top: {}, Right: {}, Bottom: {}, Left: {}".format(top, right, bottom, left))
+
+    # Draw box around face
+    draw = PIL.ImageDraw.Draw(pil_image)
+    draw.rectangle([left, top, right, bottom], outline="red")
+
+# Display the image
+pil_image.show()
