a8cd88d69a7f7c0eceffb24a1e019b3b698faba4
alexmacharia <alex.machariam@gmail.com>
2023-02-20 06:33:18 +0300

    face recognition

--- 04_face_recognition.py
+++ 04_face_recognition.py
@@ -1,9 +1,9 @@
 import face_recognition
 
 # Load the known images
-person_1 = face_recognition.load_image("person_1.jpg")
-person_2 = face_recognition.load_image("person_2.jpg")
-person_3 = face_recognition.load_image("person_3.jpg")
+person_1 = face_recognition.load_image_file("person_1.jpg")
+person_2 = face_recognition.load_image_file("person_2.jpg")
+person_3 = face_recognition.load_image_file("person_3.jpg")
 
 # Get the face encoding of each person
 person_1_encoding = face_recognition.face_encodings(person_1)[0]
